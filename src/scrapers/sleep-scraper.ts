1f5789ce7bd562e658f183a6d1aeb9700adef89c
konfortes <ronen.konfortes@riskified.com>
2020-05-23 22:17:21 +0300

    progress

--- src/scrapers/sleep-scraper.ts
+++ src/scrapers/sleep-scraper.ts
@@ -7,11 +7,20 @@ export interface SleepStats {
 }
 
 export async function sleepScraper(page: Page): Promise<SleepStats> {
+    // TODO: fix the waitFor and remove this sleep
+    await new Promise(r => setTimeout(r, 2000))
     await page.waitFor('ul.main > li:nth-child(2)')
     await page.click('ul.main > li:nth-child(2)')
+    // TODO: fix the waitFor and remove this sleep
+    await new Promise(r => setTimeout(r, 2000))
     await page.waitFor('a[href^="/modern/sleep/"]')
     await page.click('a[href^="/modern/sleep/"]')
-    const stats = await page.$eval('span.InlineEdit_label__cDTfw', el => el.textContent)
+    // TODO: fix the waitFor and remove this sleep
+    await new Promise(r => setTimeout(r, 2000))
+    const stats = await page.$eval('span.InlineEdit_label__cDTfw', el => {
+        return el.textContent
+    })
+    console.log(stats)
     return {
         bedTime: (stats || [])[0],
         wakeTime: (stats || [])[1],
