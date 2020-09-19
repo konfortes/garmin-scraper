e47c9eac03f4adf38ce8a63eb42b2a52fd0998a6
konfortes <ronen.konfortes@riskified.com>
2020-09-19 21:17:08 +0300

    hr-scraper

--- src/scrapers/sleep-scraper.ts
+++ src/scrapers/sleep-scraper.ts
@@ -9,17 +9,20 @@ export interface SleepStats {
 export async function sleepScraper(page: Page): Promise<SleepStats> {
     // TODO: fix the waitFor and remove this sleep
     await new Promise(r => setTimeout(r, 2000))
-    await page.waitFor('ul.main > li:nth-child(2)')
+    await page.waitForSelector('ul.main > li:nth-child(2)')
     await page.click('ul.main > li:nth-child(2)')
     // TODO: fix the waitFor and remove this sleep
     await new Promise(r => setTimeout(r, 2000))
-    await page.waitFor('a[href^="/modern/sleep/"]')
+    await page.waitForSelector('a[href^="/modern/sleep/"]')
     await page.click('a[href^="/modern/sleep/"]')
     // TODO: fix the waitFor and remove this sleep
     await new Promise(r => setTimeout(r, 2000))
-    const stats = await page.$eval('span.InlineEdit_label__cDTfw', el => {
+    const stats = await page.$eval('InlineEdit_label__R0jVr', el => {
         return el.textContent
     })
+    // const wakeTime = await page.$eval('InlineEdit_label__R0jVr ', el => {
+    //     return el.textContent
+    // })
     console.log(stats)
     return {
         bedTime: (stats || [])[0],
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { Page } from 'puppeteer'
 
 export async function waitAndClick(page: Page, selector: string): Promise<any> {
-    await page.waitFor(selector)
+    await page.waitForSelector(selector)
     await page.click(selector)
 }
